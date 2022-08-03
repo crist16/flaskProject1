d32ac68138d89864807052c37c0709a256fc4ffa
crist16 <cristobalnegocio@gmail.com>
2022-08-02 20:11:22 -0400

    First

--- Helpers/helper.py
+++ Helpers/helper.py
@@ -1,21 +1,6 @@
 from docxtpl import DocxTemplate
 import  time
-content = {
-    "nombreEstudiante": "Cristobal",
-    "cedulaEstudiante": "19762932",
-    "ciudadNacimiento": "Cumana",
-    "diaNacimiento": "05-05-1991",
-    "mesNacimiento": "Mayo",
-    "gradoCurso": "Tercer",
-    "literal": "A",
-    "periodoEscolar": "2022-2023",
-    "gradoPromovido": "Cuarto",
-    "nivelPromovido": "Basica",
-    "diaExpedicion": "13",
-    "mesExpedicion": "Julio",
-    "yearExpedicion": "2022"
 
-}
 
 def generarPdf(contenido):
     doc = DocxTemplate("Inputs/Templates/Prosecucion.docx")
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, send_from_directory,request
 from Helpers.helper import generarPdf
 from os import getcwd
+import os
 from datetime import datetime
 
 mesesDic = {
@@ -17,6 +18,29 @@ mesesDic = {
     "11":'Noviembre',
     "12":'Diciembre'
 }
+
+content = {
+    "nombreEstudiante": "Cristobal",
+    "cedulaEstudiante": "19762932",
+    "ciudadNacimiento": "Cumana",
+    "diaNacimiento": "05-05-1991",
+    "mesNacimiento": "Mayo",
+    "gradoCurso": "Tercer",
+    "literal": "A",
+    "periodoEscolar": "2022-2023",
+    "gradoPromovido": "Cuarto",
+    "nivelPromovido": "Basica",
+    "diaExpedicion": "13",
+    "mesExpedicion": "Julio",
+    "yearExpedicion": "2022"
+
+}
+
+def deleteOutputFiles():
+    files = os.listdir("Outputs")
+    print("Deleting")
+    for f in files:
+        os.remove("Outputs/" + f)
 
 def obtenerFechaActual():
     fecha = datetime.now()
@@ -43,16 +67,27 @@ def hello_world():  # put application's code here
 
 @app.post(f"/constancia/prosecucion")
 def exportarProsecicion():
+    deleteOutputFiles()
     fechaActual = obtenerFechaActual()
    # print(fechaActual)
+
     valores = request.get_json()
+
     valores["diaExpedicion"] = fechaActual["dia"]
     valores["mesExpedicion"] = fechaActual["mes"]
     valores["yearExpedicion"] = fechaActual["year"]
     name_file = generarPdf(valores)
     print(name_file)
-    return send_from_directory(PATH_FILE_OUTPUT,path=name_file, as_attachment=True)
+    return "received"
+
+@app.get(f"/constancia/prosecucion/<name_file>")
+def download_file(name_file):
+    return "Recibiendo archivo"
+    #return send_from_directory(PATH_FILE_OUTPUT,path=name_file, as_attachment=True)
 
 
 if __name__ == '__main__':
     app.run()
+
+
+
