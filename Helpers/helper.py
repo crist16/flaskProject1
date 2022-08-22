1fcf7a83407215772cc4174ab59a27d458f6d3df
crist16 <cristobalnegocio@gmail.com>
2022-08-22 16:56:55 -0400

    22-08 comit

--- Helpers/helper.py
+++ Helpers/helper.py
@@ -1,11 +1,81 @@
+import json
+
 from docxtpl import DocxTemplate
 import  time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import pandas as pd
 
+def enviarCorreo(path_name, file_name):
+
+    # Para configurar gmail y poder enviar correos hay que setear 2 verification y crear una custom app en la cuenta de google
+    # Iniciamos los parámetros del script
+    remitente = "bolivariano.automated@gmail.com"
+    my_pass = "rnguutslnoexyetr"
+    destinatarios = "cristobalnegocio@gmail.com"
+    asunto = '[RPI] Correo de prueba'
+    cuerpo = 'Este es el contenido del mensaje'
+    ruta_adjunto = path_name
+    nombre_adjunto = file_name
+
+    em = MIMEMultipart()
+    em['From'] = remitente
+    em['To'] = destinatarios
+    em['Subject'] = asunto
+    em.add_header('Content-Disposition', 'attachment', filename='bud.gif')
+
+    # Agregamos el cuerpo del mensaje como objeto MIME de tipo texto
+    em.attach(MIMEText(cuerpo, 'plain'))
+
+    # Abrimos el archivo que vamos a adjuntar
+    archivo_adjunto = open(ruta_adjunto, 'rb')
+
+    # Creamos un objeto MIME base
+    adjunto_MIME = MIMEBase('application', 'octet-stream')
+    # Y le cargamos el archivo adjunto
+    adjunto_MIME.set_payload((archivo_adjunto).read())
+    # Codificamos el objeto en BASE64
+    encoders.encode_base64(adjunto_MIME)
+    # Agregamos una cabecera al objeto
+    adjunto_MIME.add_header('Content-Disposition', "attachment; filename= %s" % nombre_adjunto)
+    # Y finalmente lo agregamos al mensaje
+    em.attach(adjunto_MIME)
+
+    # Creamos la conexión con el servidor
+    sesion_smtp = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # Ciframos la conexión
+    sesion_smtp.starttls()
+
+    # Iniciamos sesión en el servidor
+    sesion_smtp.login(remitente, my_pass)
+
+    # Convertimos el objeto mensaje a texto
+    texto = em.as_string()
+
+    # Enviamos el mensaje
+    sesion_smtp.sendmail(remitente, destinatarios, texto)
+
+    # Cerramos la conexión
+    sesion_smtp.quit()
 
 def generarPdf(contenido):
     doc = DocxTemplate("Inputs/Templates/Prosecucion.docx")
     doc.render(contenido)
     doc.save(f"Outputs/{contenido['nombreEstudiante']}.docx")
     time.sleep(3)
-    docx = f"Outputs/{contenido['nombreEstudiante']}.docx"
-    return f"{contenido['nombreEstudiante']}.docx"
+    path_docx = f"Outputs/{contenido['nombreEstudiante']}.docx"
+    nombre_docx = f"{contenido['nombreEstudiante']}.docx"
+    enviarCorreo(path_docx,nombre_docx)
+    return nombre_docx
+
+def obreros():
+    obreros = pd.read_csv("Inputs/Trabajadores/PersonalParaCuadraturaObreros.csv")
+
+    print(obreros["APELLIDOS Y NOMBRES"])
+    apellidos_y_nombres = obreros["EDAD"]
+    print(apellidos_y_nombres)
+    return apellidos_y_nombres
--- app.py
+++ app.py
@@ -1,41 +1,24 @@
 from flask import Flask, send_from_directory,request
-from Helpers.helper import generarPdf
+from Helpers.helper import generarPdf,obreros
 from os import getcwd
-
 from datetime import datetime
 
+
+PATH_FILE_OUTPUT = getcwd() + "/Outputs"
 mesesDic = {
-    "01":'Enero',
-    "02":'Febrero',
-    "03":'Marzo',
-    "04":'Abril',
-    "05":'Mayo',
-    "06":'Junio',
-    "07":'Julio',
-    "08":'Agosto',
-    "09":'Septiembre',
-    "10":'Octubre',
-    "11":'Noviembre',
-    "12":'Diciembre'
+    "01":'ENERO',
+    "02":'FEBRERO',
+    "03":'MARZO',
+    "04":'ABRIL',
+    "05":'MAYO',
+    "06":'JUNIO',
+    "07":'JULIO',
+    "08":'AGOSTO',
+    "09":'SEPTIEMBRE',
+    "10":'OCTUBRE',
+    "11":'NOVIEMBRE',
+    "12":'DICIEMBRE'
 }
-
-content = {
-    "nombreEstudiante": "Cristobal",
-    "cedulaEstudiante": "19762932",
-    "ciudadNacimiento": "Cumana",
-    "diaNacimiento": "05-05-1991",
-    "mesNacimiento": "Mayo",
-    "gradoCurso": "Tercer",
-    "literal": "A",
-    "periodoEscolar": "2022-2023",
-    "gradoPromovido": "Cuarto",
-    "nivelPromovido": "Basica",
-    "diaExpedicion": "13",
-    "mesExpedicion": "Julio",
-    "yearExpedicion": "2022"
-
-}
-
 
 def obtenerFechaActual():
     fecha = datetime.now()
@@ -51,35 +34,36 @@ def obtenerFechaActual():
             fechaCompletaDeHoy["mes"] = mesesDic[mes]
     return fechaCompletaDeHoy
 
-PATH_FILE_OUTPUT = getcwd() + "/Outputs"
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World Upadating!'
+    return 'Hello World '
 
 
 @app.post(f"/constancia/prosecucion")
-def exportarProsecicion():
-
+def exportarProsecucion():
     fechaActual = obtenerFechaActual()
    # print(fechaActual)
-
     valores = request.get_json()
-
     valores["diaExpedicion"] = fechaActual["dia"]
     valores["mesExpedicion"] = fechaActual["mes"]
     valores["yearExpedicion"] = fechaActual["year"]
+    for mes in  mesesDic:
+        if mes == valores["mesNacimiento"]:
+            valores["mesNacimiento"] = mesesDic[mes]
+           
     name_file = generarPdf(valores)
-
-    return "receivedd"
+    return valores["mesNacimiento"]
 
 @app.get(f"/download/<name_file>")
 def download_file(name_file):
-
     return send_from_directory(PATH_FILE_OUTPUT,path=name_file, as_attachment=True)
 
+@app.get(f"/trabajadores/obreros")
+def obrero_data():
+
+    return obreros()
 
 if __name__ == '__main__':
     app.run()
